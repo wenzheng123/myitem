a0581a02058d69d816ba9fb19f89c08c081b7cd9
lanouhn <lanouhn@lanouhndeMac-mini-62.local>
2018-09-19 09:02:46 +0800

    1

--- friday/src/router/index.js
+++ friday/src/router/index.js
@@ -17,6 +17,8 @@ import New from '@/components/New'
 import Intro1 from '@/components/Intro1'
 import Intro2 from '@/components/Intro2'
 import CountDown from '@/components/CountDown'
+import Host from '@/components/Host'
+import StoreClassify from '@/components/StoreClassify'
 import Test from '@/components/Test'
 Vue.use(Router)
 
@@ -25,7 +27,7 @@ export default new Router({
     {
       path:"/",
       name:"Tou1",
-      component: Test
+      component: StoreClassify
     },
     {
       path:"/intro",
